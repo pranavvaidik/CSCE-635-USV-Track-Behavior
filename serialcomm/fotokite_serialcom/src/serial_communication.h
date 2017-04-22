cdb3a6a8fc2a832ce24efc41de720440296cf4af
Pranav Vaidik Dhulipala <pranav.vaidik@gmail.com>
2017-04-22 14:39:45 -0500

    added the carriage sequence and checking for the Ground station response steps in the serial_comm_initialize method. The method will now also check if the serial connection with fotokite is successful.

--- serialcomm/fotokite_serialcom/src/serial_communication.h
+++ serialcomm/fotokite_serialcom/src/serial_communication.h
@@ -2,38 +2,61 @@
  * serial_communication.h
  *
  *  Created on: Apr 18, 2017
- *      Author: haresh
+ *      Author: Haresh Karnan, Pranav Vaidik Dhulipala
  */
 
 #ifndef SERIAL_COMMUNICATION_H_
 #define SERIAL_COMMUNICATION_H_
 #include <iostream>
 #include <SerialStream.h>
-#include <string>
+#include <string.h>
 #include <math.h>
 using namespace std;
 using namespace LibSerial;
 
 class serial_comm
 {
-SerialStream serial;
-public:
-    serial_comm();
-    int send_char(char);
-    void send_serial_command(string);
-    void gimbal_roll(float);
-    void gimbal_pitch(float);
-    void fotokite_yaw(float);
+    SerialStream serial;
+    public:
+        void serial_comm_initialize();
+        int send_char(char);
+        void send_serial_command(string);
+        void gimbal_roll(float);
+        void gimbal_pitch(float);
+        void fotokite_yaw(float);
 };
 
-serial_comm::serial_comm()
+void serial_comm::serial_comm_initialize()
 {
     serial.Open("/dev/ttyACM0");
     serial.SetCharSize(SerialStreamBuf::CHAR_SIZE_8);
     serial.SetBaudRate(SerialStreamBuf::BAUD_115200);
     serial.SetNumOfStopBits(1);
     serial.SetFlowControl(SerialStreamBuf::FLOW_CONTROL_NONE);
-    serial<<"RemoteControl start";
+    
+    
+    //Sending 5 carriage sequences
+    string carriage_sequence = "\r\r\r\r\r";
+    serial<< carriage_sequence;
+    
+    
+    //Look for the "nsh>" response from the Ground station
+    char ground_station_response[4];
+    serial.read(ground_station_response,4);
+    
+    if (strcmp(ground_station_response,"nsh>"))
+    {
+        cout<<"connection with Ground Station Established Successfully";
+        serial<<"RemoteControl start";
+        serial<<"";
+    }
+    else
+    {
+        cout<<"connection failed";
+        return;
+    }
+    
+    
 }
 
 void serial_comm::send_serial_command(string str){
